e932516f469d82a52d8248cb0d6d4f9c26bc50fa
M. Hasyim Abdillah P. <abdillahpronosumarto@gmail.com>
2023-10-17 12:26:47 +0800

    update Homepage.py

--- 1_🏠Homepage.py
+++ 1_🏠Homepage.py
@@ -3,8 +3,8 @@ import base64
 import streamlit as st
 
 from PIL import Image
-from pathlib import Path
-from streamlit_gsheets import GSheetsConnection
+# from pathlib import Path
+# from streamlit_gsheets import GSheetsConnection
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 icon = Image.open(BASE_DIR + '/images/logo_TBSC.jpeg')
