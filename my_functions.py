28be9ce9a4449d70c5a17fd0a629d471e3f8dad8
M. Hasyim Abdillah P. <abdillahpronosumarto@gmail.com>
2023-12-29 20:03:26 +0800

    add my_functions.py

---
+++ my_functions.py
@@ -0,0 +1,58 @@
+def assign_rank(score):
+    if score > 98.0:
+        return 'EX'
+    elif score > 95:
+        return 'S+'
+    elif score > 92.5:
+        return 'S'
+    elif score > 90:
+        return 'S-'
+    elif score > 87:
+        return 'A+'
+    elif score > 83:
+        return 'A'
+    elif score > 80:
+        return 'A-'
+    elif score > 77:
+        return 'B+'
+    elif score > 73:
+        return 'B'
+    elif score > 70:
+        return 'B-'
+    elif score > 67:
+        return 'C+'
+    elif score > 63:
+        return 'C'
+    elif score > 60:
+        return 'C-'
+    elif score > 57:
+        return 'D+'
+    elif score > 53:
+        return 'D'
+    elif score > 50:
+        return 'D-'
+    elif score > 45:
+        return 'E+'
+    elif score > 35:
+        return 'E'
+    elif score > 25:
+        return 'E-'
+    else:
+        return 'F'
+    
+def categorize_age(age):
+    if age >= 19:
+        return 'Senior'
+    elif age >= 16:
+        return 'AG 1'
+    elif age >= 14:
+        return 'AG 2'
+    elif age >= 12:
+        return 'AG 3'
+    elif age >= 10:
+        return 'AG 4'
+    elif age >= 8:
+        return 'AG 5'
+    else:
+        return 'Beginner'
+
