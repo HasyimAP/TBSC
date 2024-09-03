789c013ad399d679f20acd5161fe1ba5027def04
hasyim_abdillah <hasyim.abdillah@nucleusbi.com>
2024-09-03 15:43:15 +0800

    update 233Pacing Chart

--- pages/5_⌛Pacing Chart.py
+++ pages/5_⌛Pacing Chart.py
@@ -55,6 +55,44 @@ background-color: rgba(197, 239, 247, 0.75);
 </style>
 '''
 
+def get_chart(event: str):
+    best_time = df_best_time[df_best_time['Event'] == event]['Record (s)']
+    
+    match = re.search(r'(\d+)m?', event)
+    distance = int(match.group(1))
+
+    best_speed = distance/best_time
+    target_speed = 1.02 * best_speed
+    target_time = round(distance/target_speed, 2)
+    diff = best_time-target_time
+
+    pace_chart = pd.DataFrame(columns=[event])
+    pace_chart[event] = [f'{x}%' for x in range(100, 59, -5)]
+    pace_chart['Level'] = ['Gold Speed', 'ANA 2', 'ANA 1', 'Vo2max+', 'Vo2max', 'AT+', 'AT', 'AE 2', 'AE 1']
+    pace_chart['Personal best (s)'] = pace_chart[event].apply(lambda x: round(best_time + best_time*(1-((float(x.strip('%'))/100))), 2))
+    pace_chart['Target time (s)'] = pace_chart[event].apply(lambda x: round(target_time + target_time*(1-((float(x.strip('%'))/100))), 2))
+    pace_chart['Diff from PB'] = pace_chart['Personal best (s)'].apply(lambda x: f'+ {round(x - pace_chart[pace_chart[event] == "100%"]["Personal best (s)"].values[0], 2)}')
+    pace_chart['Diff from TT'] = pace_chart['Target time (s)'].apply(lambda x: f'+ {round(x - pace_chart[pace_chart[event] == "100%"]["Target time (s)"].values[0], 2)}')
+
+    pace_chart['PB min'] = pace_chart['Personal best (s)'] // 60
+    pace_chart['PB sec'] = pace_chart['Personal best (s)'] % 60
+    pace_chart['Personal best'] = pace_chart['PB min'].apply(lambda x: f'{int(x):02}') + ':' + pace_chart["PB sec"].apply(lambda x: f'{x:05.2f}')
+
+    pace_chart['TT min'] = pace_chart['Target time (s)'] // 60
+    pace_chart['TT sec'] = pace_chart['Target time (s)'] % 60
+    pace_chart['Target time'] = pace_chart['TT min'].apply(lambda x: f'{int(x):02}') + ':' + pace_chart["TT sec"].apply(lambda x: f'{x:05.2f}')
+
+    pace_chart = pace_chart[[
+        'Level',
+        event,
+        'Personal best',
+        'Diff from PB',
+        'Target time',
+        'Diff from TT'
+    ]]
+
+    return pace_chart
+
 st.markdown(page_bg_img, unsafe_allow_html=True)
 
 # st.title('Coming Soon...')
@@ -137,6 +175,7 @@ with col2:
 
 pace_chart = pd.DataFrame(columns=[event])
 pace_chart[event] = [f'{x}%' for x in range(100, 59, -5)]
+pace_chart['Level'] = ['Gold Speed', 'ANA 2', 'ANA 1', 'Vo2max+', 'Vo2max', 'AT+', 'AT', 'AE 2', 'AE 1']
 pace_chart['Personal best (s)'] = pace_chart[event].apply(lambda x: round(best_time + best_time*(1-((float(x.strip('%'))/100))), 2))
 pace_chart['Target time (s)'] = pace_chart[event].apply(lambda x: round(target_time + target_time*(1-((float(x.strip('%'))/100))), 2))
 pace_chart['Diff from PB'] = pace_chart['Personal best (s)'].apply(lambda x: f'+ {round(x - pace_chart[pace_chart[event] == "100%"]["Personal best (s)"].values[0], 2)}')
@@ -151,6 +190,7 @@ pace_chart['TT sec'] = pace_chart['Target time (s)'] % 60
 pace_chart['Target time'] = pace_chart['TT min'].apply(lambda x: f'{int(x):02}') + ':' + pace_chart["TT sec"].apply(lambda x: f'{x:05.2f}')
 
 pace_chart = pace_chart[[
+    'Level',
     event,
     'Personal best',
     'Diff from PB',
@@ -161,6 +201,12 @@ pace_chart = pace_chart[[
 st.dataframe(
     pace_chart,
     hide_index=True
+)
+
+events = st.multiselect(
+    'Choose event(s):',
+    options=sorted(df_best_time['Event'].unique()),
+    placeholder='Choose 1 or more'
 )
 
 col3, col4 = st.columns(2)
@@ -178,9 +224,49 @@ with col3:
         )
 
 with col4:
-    def df_to_pdf(dataframe: pd.DataFrame):
+    def create_watermark(pdf: FPDF):
+        watermark_image = 'images/logo_TBSC.jpeg'
+        image = Image.open(watermark_image).convert("RGBA")
+    
+        alpha = image.split()[3]
+        alpha = alpha.point(lambda p: p * 0.2) 
+        image.putalpha(alpha)
+        
+        with tempfile.NamedTemporaryFile(delete=False, suffix='.png') as temp_file:
+            image.save(temp_file.name, format='PNG')
+            watermark_image = temp_file.name
+
+        with Image.open(watermark_image) as img:
+            watermark_width, watermark_height = img.size
+        
+        if watermark_width > watermark_height:
+            scale = pdf.w / watermark_width
+            new_width = pdf.w/2
+            new_height = (watermark_height * scale)/2
+        else:
+            scale = pdf.h / watermark_height
+            new_width = (watermark_width * scale)/2
+            new_height = pdf.h/2
+        
+        x_position = (pdf.w - new_width) / 2
+        y_position = (pdf.h - new_height) / 2
+        
+        wm = {
+            'wm': watermark_image,
+            'x': x_position,
+            'y': y_position,
+            'w': new_width,
+            'h': new_height
+        }
+        return wm
+
+    def df_to_pdf(event_list: list[str]):
         pdf = FPDF()
         pdf.add_page()
+
+        wm_img = create_watermark(pdf)
+
+        pdf.image(wm_img['wm'], x=wm_img['x'], y=wm_img['y'], w=wm_img['w'], h=wm_img['h'])
         
         # Font and size
         pdf.set_font('Arial', size=12)
@@ -189,34 +275,50 @@ with col4:
         pdf.cell(200, 10, txt=athlete, ln=True, align='C')
         pdf.ln(5)
 
-        col_widths = [max(pdf.get_string_width(str(dataframe[col].max()))+6, pdf.get_string_width(col)+6) for col in dataframe.columns]
-        total_width = sum(col_widths) + len(col_widths)
-        page_width = pdf.w - pdf.l_margin - pdf.r_margin
-        start_x = (page_width - total_width)
+        for i_event, event in enumerate(event_list):
+            dataframe = get_chart(event)
 
-        pdf.set_x(start_x)
+            col_widths = [max(pdf.get_string_width(str(dataframe[col].max()))+6, pdf.get_string_width(col)+6) for col in dataframe.columns]
+            total_width = sum(col_widths) + len(col_widths)
+            page_width = pdf.w - pdf.l_margin - pdf.r_margin
+            start_x = pdf.l_margin + (page_width/2 - total_width/2)
 
-        # Column names 
-        pdf.set_font('Arial', 'B', size=12)  # Bold font for headers
-        for i, col in enumerate(dataframe.columns):
-            pdf.cell(col_widths[i], 10, col, 1, align='C')
-        pdf.ln()
-        
-        # Row data
-        pdf.set_font('Arial', size=12)  # Regular font for data
-        for index, row in dataframe.iterrows():
             pdf.set_x(start_x)
+
+            # Column names 
+            pdf.set_font('Arial', 'B', size=12)  # Bold font for headers
             for i, col in enumerate(dataframe.columns):
-                pdf.cell(col_widths[i], 10, str(row[col]), 1, align='C')
+                pdf.cell(col_widths[i], 10, col, 1, align='C')
             pdf.ln()
+            
+            # Row data
+            pdf.set_font('Arial', size=12)  # Regular font for data
+            for _, row in dataframe.iterrows():
+                pdf.set_x(start_x)
+                for i, col in enumerate(dataframe.columns):
+                    pdf.cell(col_widths[i], 10, str(row[col]), 1, align='C')
+                pdf.ln()
+
+            pdf.ln(20)
+
+            if i_event % 2 == 0:
+                continue
+            elif i_event == len(event_list)-1:
+                break
+            else:
+                pdf.add_page()
+                pdf.cell(200, 10, txt=athlete, ln=True, align='C')
+                pdf.ln(5)
+                
+                pdf.image(wm_img['wm'], x=wm_img['x'], y=wm_img['y'], w=wm_img['w'], h=wm_img['h'])
         
         return pdf.output(dest='S').encode('latin1')
     
-    pdf_bytes = df_to_pdf(pace_chart)
+    pdf_bytes = df_to_pdf(events)
 
     st.download_button(
         label="Download chart as PDF",
         data=pdf_bytes,
-        file_name=f'{athlete} {event}.pdf',
+        file_name=f'{athlete}.pdf',
         mime='application/pdf'
     )
