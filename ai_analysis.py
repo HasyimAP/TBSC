26295c25ee056f3978cb6b63260f646aa290092f
hasyim_abdillah <hasyim.abdillah@nucleusbi.com>
2025-03-10 12:51:26 +0800

    update ai_analysis

--- ai_analysis.py
+++ ai_analysis.py
@@ -11,11 +11,11 @@ class AthleteSwot(BaseModel):
     weaknesses: str = Field(..., description='Athlete weakness points. What are they bad at?')
     opportunities: str = Field(..., description='Athlete opportunities. What can they improve on?')
     threats: str = Field(..., description='Athlete threats. What can hinder their performance?')
-    best_stroke: str = Field(..., description='Athlete best stroke based on the available data. Should be 1 of the following: freestyle, backstroke, breaststroke, butterfly.')
-    best_distance: str = Field(..., description='Athlete best distance based on the available data. What is their best distance?. The distance should be 1 of the following: sprint, middle distance, long distance.')
-    weakest_stroke: str = Field(..., description='Athlete weakest stroke based on the available data. Should be 1 of the following: freestyle, backstroke, breaststroke, butterfly.')
-    weakest_distance: str = Field(..., description='Athlete weakest distance based on the available data. Should be 1 of the following: sprint, middle distance, long distance.')
-    medley: str = Field(..., description='Based on the available data, should the athlete focus on medley events? Yes or No. Explain why.')
+    best_stroke: str = Field(..., description='Athlete best stroke based on the available data. Should be 1 of the following: freestyle, backstroke, breaststroke, butterfly. Explain shortly why.')
+    best_distance: str = Field(..., description='Athlete best distance based on the available data. What is their best distance?. The distance should be 1 of the following: sprint, middle distance, long distance. Explain shortly why.')
+    weakest_stroke: str = Field(..., description='Athlete weakest stroke based on the available data. Should be 1 of the following: freestyle, backstroke, breaststroke, butterfly. Must be different from the best stroke. Explain shortly why.')
+    weakest_distance: str = Field(..., description='Athlete weakest distance based on the available data. Should be 1 of the following: sprint, middle distance, long distance. Must be different from the best distance. Explain shortly why.')
+    medley: str = Field(..., description='Based on the available data, should the athlete focus on medley events? Yes, No, or Maybe. Explain why.')
     specialization: str = Field(..., description='Athlete specialization. Considering their age, potential growth, and current performance, should they start specializing in a specific stroke or distance? Yes, No, or Maybe. Prioritize Yes or No, only use Maybe if you are unsure. For younger athletes, it is recommended to focus on all strokes and distances. For older athletes, it is recommended to start specializing. Explain why.')
     development_plan: str = Field(..., description='Athlete development plan. What kind of swimmer should they aim to be? Is it a sprinter, distance swimmer, all-rounder, or anything else?')
     short_term_goals: str = Field(..., description='Athlete short term goals. What should they aim to achieve in the next 6 months or in the minor competitions? Should be specific, measurable, achievable, relevant, and time-bound.')
@@ -32,9 +32,6 @@ def analyze_athlete_stats(bio: str, stats: dict):
 
     Here is the athelete's statistics that you will analyze:
     {stats}
-
-    The output should be modeled after this Pydanctic data model schema in JSON format:
-    {AthleteSwot.model_json_schema()}
     """
 
     client = genai.Client(api_key=st.secrets.gemini_llm.api_key)
@@ -45,10 +42,10 @@ def analyze_athlete_stats(bio: str, stats: dict):
         config=types.GenerateContentConfig(
             response_mime_type='application/json',
             response_schema=AthleteSwot,
-            temperature=0.5,
+            temperature=0.2,
+            top_p=0.8,
+            top_k=40,
         )
     )
 
     return response
-
-# st.write('AI persona:', st.secrets.gemini_llm.persona)
