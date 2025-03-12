2670aa7d313c13b57fe929817e6436d2d1322ca8
hasyim_abdillah <hasyim.abdillah@nucleusbi.com>
2025-03-12 14:57:36 +0800

    update competition
    add level column to competition table

--- competition.py
+++ competition.py
@@ -6,15 +6,18 @@ from streamlit_gsheets import GSheetsConnection
 conn = st.connection("gsheets", type=GSheetsConnection)
 
 df_competition = conn.read(worksheet='Competitions')
-df_records = conn.read(worksheet='Records', usecols=list(range(0,7))).dropna(axis=0, how='all')
+df_records = conn.read(worksheet='Records').dropna(axis=0, how='all').dropna(axis=1, how='all')
 
+drop = ['DNS', 'DQ', 'NS', 'NSS']
 comp_list = df_records['Competition'].unique()
+level = []
 start_date = []
 end_date = []
 total_athletes = []
 
 for comp in comp_list:
     df_temp = df_records[df_records['Competition'] == comp]
+    level.append(df_temp['Level'].unique()[0])
     start_date.append(df_temp['Date'].min())
     end_date.append(df_temp['Date'].max())
     total_athletes.append(df_temp['Name'].nunique())
@@ -22,6 +25,7 @@ for comp in comp_list:
 df_competition = pd.DataFrame()
 
 df_competition['Competition'] = comp_list
+df_competition['Level'] = level
 df_competition['Start Date'] = start_date
 df_competition['End Date'] = end_date
 df_competition['Total Athletes'] = total_athletes
